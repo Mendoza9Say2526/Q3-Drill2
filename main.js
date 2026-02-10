b8feccac0e76e8627bb07a79b966cf139328ca2f
Mendoza9Say2526 <mendoza.jm@obmontessori.edu.ph>
2026-02-10 08:50:44 +0800

    Remove extra blank lines in main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-window.alert("Welcome to the CACulator. Enter the radius so you can get the area of the circle.")
+window.alert("Welcome to the CACulator. Enter the radius so you can get the area of the circle.");
 
 function calculateArea(radius) {
 var rad = document.getElementById("rad").value;
 var area = Math.PI * (rad ** 2);
 document.getElementById("area").value = area;
 }
+
 
 
 
