8e6abb7735716c0e9ad10e97d9cf3788dcffdfe9
Mendoza9Say2526 <mendoza.jm@obmontessori.edu.ph>
2026-02-10 08:09:54 +0800

    Refactor area calculation to use '**' operator
    Updated area calculation to use exponentiation operator.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function calculateArea(radius) {
 var rad = document.getElementById("rad").value;
-var area = Math.PI * Math.pow(rad, 2);
+var area = Math.PI * Math.pow(rad ** 2);
 document.getElementById("area").value = area;
 }
+
 
 
 
